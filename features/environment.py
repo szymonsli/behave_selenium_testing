86d2eb881b8af7c57006cc1fb8e9b4f2c08d721a
szymonsli <szymonsli@users.noreply.github.com>
2021-08-04 07:58:08 +0200

    Added environment file

---
+++ features/environment.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+
+def before_feature(context, feature):
+    context.BASE_URL = "http://automationpractice.com"
+    context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(2)
+
+
+def after_feature(context, feature):
+    context.driver.quit()

8ed31cc15ae2fe539d36a7e67b195a11c84832e6
szymonsli <szymonsli@users.noreply.github.com>
2021-08-04 09:18:26 +0200

    chromedriver travis test

--- features/environment.py
+++ features/environment.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 def before_feature(context, feature):
     context.BASE_URL = "http://automationpractice.com"
-    context.driver = webdriver.Chrome()
+    context.driver = webdriver.Chrome('./../chromedriver.exe')
     context.driver.maximize_window()
     context.driver.implicitly_wait(2)
 

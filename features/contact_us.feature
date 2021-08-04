7a0183fabf91252eb6709fed7c914e6355118c66
szymonsli <szymonsli@users.noreply.github.com>
2021-08-04 09:00:10 +0200

    Refactored first test

--- features/contact_us.feature
+++ features/contact_us.feature
@@ -5,4 +5,5 @@ Feature: Contact us form
   Scenario: sending a contact form without files attached
     Given user visits automationpractice.com Contact Us site
     When user fills in the form with valid email, message and subject
+    And clicks submit button
     Then contact form is sent and user sees success alert
--- features/steps/contact_us_steps.py
+++ features/steps/contact_us_steps.py
@@ -5,12 +5,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 
 
 @given('user visits automationpractice.com Contact Us site')
-def step_impl(context):
+def visit_contact_us(context):
     context.driver.get(f"{context.BASE_URL}/index.php?controller=contact")
 
 
 @when('user fills in the form with valid email, message and subject')
-def step_impl(context):
+def fill_form_correctly(context):
     email_input = context.driver.find_element(By.ID, "email")
     message_input = context.driver.find_element(By.ID, "message")
     email_input.send_keys("example@example.com")
@@ -19,11 +19,15 @@ def step_impl(context):
     subject_dropdown.click()
     option = subject_dropdown.find_element(By.XPATH, "//option[2]")
     option.click()
+
+
+@when('clicks submit button')
+def click_submit_button(context):
     context.driver.find_element(By.ID, "submitMessage").click()
 
 
 @then('contact form is sent and user sees success alert')
-def step_impl(context):
+def contact_form_is_sent(context):
     alert = WebDriverWait(context.driver, timeout=10).until(
         EC.visibility_of_element_located((By.CLASS_NAME, "alert-success"))
     )

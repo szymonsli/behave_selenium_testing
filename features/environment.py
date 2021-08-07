85abc8db7eb63d6ebd6178f261efe74e44c63d97
szymonsli <szymonsli@users.noreply.github.com>
2021-08-07 18:20:17 +0200

    One more try with travis

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+# import os
+
+# path = os.getcwd() + r"\drivers\chromedriver.exe"
+# print(path)
 
 
 def before_feature(context, feature):
     context.BASE_URL = "http://automationpractice.com"
-    context.driver = webdriver.Chrome('./../chromedriver.exe')
+    context.driver = webdriver.Chrome(ChromeDriverManager().install())
     context.driver.maximize_window()
     context.driver.implicitly_wait(2)
 

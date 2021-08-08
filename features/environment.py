627ae0c0269c17e2e7e478a83f9733fa2d7c9900
szymonsli <szymonsli@users.noreply.github.com>
2021-08-08 12:04:21 +0200

    Removed unnecessary code from environment.py

--- features/environment.py
+++ features/environment.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-# import os
-
-# path = os.getcwd() + r"\drivers\chromedriver.exe"
-# print(path)
 
 
 def before_feature(context, feature):
